03b466c46387788a1e93252c0989a5a10b6c5bef
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-06 18:49:32 +0530

    typecasting notes

---
+++ input.py
@@ -0,0 +1,15 @@
+# input() --- allows a program/ code to receive information from a user
+
+name = input("Enter your name:")
+print(f"Hello, {name}")
+
+age = input("Enter your age:")
+print("age")
+
+print(type(age))
+
+# WHENEVER YOU TYPE NUMBERS IN TERMINAL IT WILL SHOW STRING TO CONVERT THAT USE
+
+intage = int(age)  #int(str)
+print(type(intage))
+print(f"Age of {name} is {age}")
