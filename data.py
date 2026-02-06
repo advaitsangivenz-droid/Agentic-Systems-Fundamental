14ed0ee4543eae00e67b090c123946131255ad05
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-06 17:20:00 +0530

    DATA TYPE EXPLANATION

---
+++ data.py
@@ -0,0 +1,55 @@
+age = 30
+temperature = 21
+count = 10
+
+## when to use integers
+# counting numbers
+# indexing
+# loops
+
+# Floating point (float)
+# floating point is integer with decimals 
+
+price = 0.99
+accuracy = 97.09
+temperature = -5.12
+
+# When float is used?
+# petrol price 
+# bill
+# probabilities
+# fractions
+# measurements
+
+# What is String? (str)
+# Strings represent texts,  then are enclosed with quotes.
+
+message = "Hello coding world"
+name = "Advait"
+space = " "
+
+# Why strings used?
+# Text Processing
+# Prompts
+# name and city
+# pythonn string are inmutable
+
+full_message = "Hello " + name + space + message
+print (full_message)
+
+
+
+#bolean (bool)
+# It represents truth value
+
+is_active = True
+has_access = False
+
+# boleans are the backbone of decision making in program, they are used in conditions, filters and logic
+
+# You can check variable type using "type"
+
+print(type(is_active))
+
+
+
