7535bbc35d294ef506660e461efc3ad2aa45379d
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-06 17:57:06 +0530

    Understanding basic math operators

---
+++ math.py
@@ -0,0 +1,48 @@
+# basic mathinn python
+# operators in python
+
+a = 10 
+b = 5
+
+sum = a + b    #(addition)
+print(sum)
+
+sub = a - b      #(subtraction)
+print(sub)
+
+mul = a * b  #(multiplication)
+print(mul)
+
+div = a / b  #(division)
+print(div)
+
+
+# floor ------> nearest lowest integer to corresponding to float value
+# 3.333 -----> 3
+
+# ceil -------> nearset greater integer correspond to float value 
+# 3.333 -----> 4
+
+nearest_int_div = a // b #(Quotient)
+print(nearest_int_div)
+
+# 11 / 3
+# quotient = 3
+# remainder = 2
+
+remainder = a % b  #(remainder)
+print(remainder)
+
+
+c = 3
+d = 4
+# 3 to power 4
+
+exponent = 3 ** 4
+print(exponent)
+
+
+price = 50
+quantity = 4
+total = price * quantity
+print(total)
