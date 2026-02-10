4d4b73cb908d3f7766340e7967c5b8a0429358dd
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-10 17:40:52 +0530

    basics of using boolean statement

---
+++ Session_5/boolean.py
@@ -0,0 +1,18 @@
+user_logged_in = True
+if user_logged_in:
+    print("hello User")
+
+# <=, >=, < , > , ==
+
+if user_logged_in == True:
+    print("hey user")
+
+
+atm_pin = 1234
+pin_input = 2345
+
+if atm_pin == pin_input:
+    print("pin accepted")
+else:
+    print("pin rejected")    
+
