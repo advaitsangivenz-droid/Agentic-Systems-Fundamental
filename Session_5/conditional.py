352431f8bc588a686efee8c8d59a42d3081506a5
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-10 17:41:23 +0530

    basic conditional statement teaching

---
+++ Session_5/conditional.py
@@ -0,0 +1,38 @@
+marks = 50
+
+
+# how to write a conditional statement
+
+if marks >= 40 :
+    print("Student has passed")
+else:
+    print("student has failed")
+
+
+age = 16
+if age >= 18:
+    print("he's an adult")
+
+
+score = 82
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+else:
+    print("Grade C")
+
+
+amount = 1200
+
+if amount > 1100:
+    amount_to_be_paid = amount - 100
+    print(amount_to_be_paid)
+else:
+    print("amount")
+
+
+
+
+
