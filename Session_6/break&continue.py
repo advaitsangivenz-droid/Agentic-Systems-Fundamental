77c25d5197456d3416358f392566fd9457e2a5a7
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-13 22:18:25 +0530

    break&continue learning

---
+++ Session_6/break&continue.py
@@ -0,0 +1,20 @@
+values = {10, 12, -1, 15}
+
+# Kep printing values until -1 is found
+
+for i in values: 
+    if i == -1:
+        break
+    else:
+        print(i)
+
+
+
+
+
+
+for i in values: 
+    if i == -1:
+        continue
+    else:
+        print(i)
