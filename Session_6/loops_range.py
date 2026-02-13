759815514ebf4542b429485d2ae6c2c218236761
advaitsangivenz-droid <advaitsangivenz@gmail.com>
2026-02-13 21:29:31 +0530

    how to use loops for range

---
+++ Session_6/loops_range.py
@@ -0,0 +1,29 @@
+# Imagine a teacher checking your answer sheet
+# Take one paper
+# Check it, move to the next
+# STop when paerps are finished
+
+# loop variable in (0,1,2,3,4)
+for i in range(5):
+    print(i)
+
+# repeat 3 times
+# each time print checked
+# python handles the counting
+
+
+# you don't need to mention the counts
+
+#print 1 to 10
+
+for i in range(1,11):
+    print(i)
+
+# print (1,3,5,7,9)
+for i in range(1, 10, 2):   # start, stop, step
+    print(i)
+
+# print negative numbers from -1 to -5
+
+for i in range(5, -3, -1):
+    print(i)
